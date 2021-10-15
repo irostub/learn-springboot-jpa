b12a1b91b3258528a08174869b3c31a4bb5c9d0c
irostub <irostub@gmail.com>
2021-10-16 00:23:13 +0900

    Init commit

---
+++ src/main/java/com/irostub/learnspringbootjpa/domain/Member.java
@@ -0,0 +1,20 @@
+package com.irostub.learnspringbootjpa.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ"
+)
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Entity
+public class Member {
+    @Setter(AccessLevel.PRIVATE)
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+    private Long id;
+
+    private String name;
+}
