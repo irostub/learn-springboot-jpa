463b68e246f6f84bc0d182e23f6c411903406cb5
irostub <irostub@gmail.com>
2021-10-19 19:38:56 +0900

    Add constructor in book

--- src/main/java/com/irostub/learnspringbootjpa/domain/item/Book.java
+++ src/main/java/com/irostub/learnspringbootjpa/domain/item/Book.java
@@ -1,11 +1,13 @@
 package com.irostub.learnspringbootjpa.domain.item;
 
+import com.irostub.learnspringbootjpa.domain.CategoryItem;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
@@ -13,4 +15,10 @@ import javax.persistence.Entity;
 public class Book extends Item{
     private String author;
     private String isbn;
+
+    public Book(Long id, String name, Integer price, Integer stockQuantity, List<CategoryItem> categoryItems, String author, String isbn) {
+        super(id, name, price, stockQuantity, categoryItems);
+        this.author = author;
+        this.isbn = isbn;
+    }
 }
