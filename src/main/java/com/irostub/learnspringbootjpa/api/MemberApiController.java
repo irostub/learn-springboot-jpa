6df5decd6937c7cd87e06eeca38f2f18a7166efd
irostub <irostub@gmail.com>
2021-10-23 23:35:00 +0900

    Implement inquire member  api in controller

--- src/main/java/com/irostub/learnspringbootjpa/api/MemberApiController.java
+++ src/main/java/com/irostub/learnspringbootjpa/api/MemberApiController.java
@@ -2,11 +2,14 @@ package com.irostub.learnspringbootjpa.api;
 
 import com.irostub.learnspringbootjpa.domain.Member;
 import com.irostub.learnspringbootjpa.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -28,9 +31,38 @@ public class MemberApiController {
 
     @PutMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(@PathVariable Long id, @RequestBody @Valid UpdateMemberRequest request) {
-        memberService.updateMemberName(id ,request.getName());
+        memberService.updateMemberName(id, request.getName());
         Member member = memberService.findOne(id);
         return new UpdateMemberResponse(member.getId(), member.getName());
+    }
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result<List<MemberDto>> membersV2() {
+        List<MemberDto> result = memberService.findMembers().stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result<>(result);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    static class MemberDto {
+        private String name;
+
+        public MemberDto(String name) {
+            this.name = name;
+        }
     }
 
     @Data
@@ -39,7 +71,7 @@ public class MemberApiController {
     }
 
     @Data
-    static class CreateMemberResponse{
+    static class CreateMemberResponse {
         private Long id;
 
         public CreateMemberResponse(Long id) {
